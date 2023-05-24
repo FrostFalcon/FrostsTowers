efc63b95b24d9370b0b639432a6dcf1955d480f7
Justin Fisher <justin.b.fisher@gmail.com>
2023-05-24 11:51:06 -0400

    added

---
+++ ModHelperData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrostsTowers
+{
+    public static class ModHelperData
+    {
+        public const string Name = "Frost's Towers (Spark Tower)";
+        public const string Version = "1.0.0";
+        public const string WorksOnVersion = "36.3";
+
+        public const string Description = "Adds a new tower to the game: a primary tower themed around electricity";
+        public const string Icon = "ModIcon.png";
+
+        public const string RepoName = "FrostsTowers";
+        public const string RepoOwner = "FrostFalcon";
+    }
+}
